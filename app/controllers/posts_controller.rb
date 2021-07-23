a477cd3f07820fd32e5290acd494778682728202
EC2 Default User <ec2-user@ip-172-31-36-237.ap-northeast-1.compute.internal>
2021-07-23 08:15:05 +0000

    コメント機能実装CSS編集前

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 	# before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+	before_action :set_post, only: [:show, :edit]
 	
 	def index
 		@posts = Post.includes(:user).order("created_at DESC")
@@ -12,11 +13,11 @@ class PostsController < ApplicationController
 		redirect_to "/"
 	end
 	def show
-		@post = Post.find(params[:id])
 		@user = User.all
+		@comment = Comment.new
+		@comments = @post.comments.includes(:user)
 	end
 	def edit
-		@post = Post.find(params[:id])
 	end
 	def update
 		post = Post.find(params[:id])
@@ -32,5 +33,7 @@ class PostsController < ApplicationController
 		params.require(:post).permit(:title, :image, :introduction, :production_period,
 									:material, :tool, :budget,:important).merge(user_id: current_user.id)
   end
-	
+	def set_post
+		@post = Post.find(params[:id])
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
    has_many :posts
+   has_many :comments
 end
